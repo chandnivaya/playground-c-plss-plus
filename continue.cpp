b6113e909437b5f6607c90d82ea2119fb196d4d2
chandnivaya <vaya_chandni@yahoo.in>
2016-03-11 22:22:30 +0400

    initial commit
    the c++ code with continue and while loop

---
+++ continue.cpp
@@ -0,0 +1,19 @@
+    #include <iostream>
+    
+    using namespace std;
+    
+    int main()
+    {
+        
+      int a=0;
+      while(a++ < 10)
+      {
+          
+          if(a==4)
+          continue;
+          cout << a << " ";
+                    
+      }
+      
+       return 0;
+    }
